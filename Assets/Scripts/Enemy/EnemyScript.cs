8f7f1676d1b607be69dc8a7714357777e1f8bd5a
Hare8563 <haresaku1024@gmail.com>
2015-02-06 10:30:24 +0900

    弓兵追加

--- Assets/Scripts/Enemy/EnemyScript.cs
+++ Assets/Scripts/Enemy/EnemyScript.cs
@@ -28,6 +28,16 @@ public class EnemyScript : MonoBehaviour
     private AudioClip expGetSe;
 
     /// <summary>
+    /// 敵のタイプ
+    /// </summary>
+    [SerializeField]
+    private enum Type
+    {
+        sword = 0,
+        magic,
+        bow,
+    }
+    /// <summary>
     /// 初期化
     /// </summary>
     void Start()
